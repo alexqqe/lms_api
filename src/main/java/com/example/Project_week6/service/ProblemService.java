2dd998a3798ee2d88bf9efd12e77d91d9b218f5d
Deoksieze <husenko21@gmail.com>
2025-03-15 17:25:55 +0300

    Added checking mistakes in StudentService.java

--- src/main/java/com/example/Project_week6/service/ProblemService.java
+++ src/main/java/com/example/Project_week6/service/ProblemService.java
@@ -1,4 +1,12 @@
 package com.example.Project_week6.service;
 
+import com.example.Project_week6.model.ProblemDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProblemService {
+
+    public ProblemDto getProblem(long id){
+        return new ProblemDto(123, "bla", "bla");
+    }
 }
--- src/main/java/com/example/Project_week6/service/StudentService.java
+++ src/main/java/com/example/Project_week6/service/StudentService.java
@@ -1,21 +1,38 @@
 package com.example.Project_week6.service;
 
+import com.example.Project_week6.ecxeption.HttpStatusException;
 import com.example.Project_week6.model.StudentCreationDto;
 import com.example.Project_week6.model.StudentDto;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@Getter
+@Service
 public class StudentService {
     private final Map<Long, StudentDto> data;
+    private List<Long> deletedId;
+    @Autowired
+    private ProblemService problemService;
 
     public StudentService(){
         this.data = new HashMap<>();
+        this.deletedId = new ArrayList<>();
     }
 
     public StudentDto createStudent(StudentCreationDto studentCreationDto){
-        long newId = data.size();
-
+        long newId;
+        if (!this.deletedId.isEmpty()){
+            newId = this.deletedId.getFirst();
+        } else {
+            newId = data.size();
+        }
         StudentDto newStudent = new StudentDto(newId, studentCreationDto.getLogin(),
                 studentCreationDto.getFirstName(), studentCreationDto.getLastName(),
                 studentCreationDto.getPhoneNumber());
@@ -26,10 +43,19 @@ public class StudentService {
     }
 
     public StudentDto getStudent(long id){
+        if (!this.data.containsKey(id)){
+            throw new HttpStatusException(HttpStatus.NOT_FOUND, "Student with id = %s not found".formatted(id));
+        }
             return data.get(id);
     }
 
     public StudentDto solvingTask(long studentId, long problemId){
+        if (!this.data.containsKey(studentId)){
+            throw new HttpStatusException(HttpStatus.NOT_FOUND, "Student with id = %s not found".formatted(studentId));
+        }
+        if (problemService.getProblem(problemId) != null){
+            throw new HttpStatusException(HttpStatus.NOT_FOUND, "Student with id = %s not found".formatted(studentId));
+        }
         StudentDto student = data.get(studentId);
         student.getSolvedProblems().add(problemId);
         data.put(studentId, student);
@@ -37,8 +63,12 @@ public class StudentService {
     }
 
     public StudentDto deleteStudent(long id){
+        if (!this.data.containsKey(id)){
+            throw new HttpStatusException(HttpStatus.NOT_FOUND, "Student with id = %s not found".formatted(id));
+        }
         StudentDto student = data.get(id);
         data.remove(id);
+        this.deletedId.add(id);
         return student;
     }
 }
