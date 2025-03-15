9a1e7b54b234bd1bb109696482cfbba3b260f933
Александр Скоморохов <skomorohovaleksandr52@gmail.com>
2025-03-15 16:22:48 +0300

    initial

--- src/main/java/com/example/Project_week6/controller/ProblemController.java
+++ src/main/java/com/example/Project_week6/controller/ProblemController.java
@@ -1,4 +1,16 @@
 package com.example.Project_week6.controller;
 
+@RestController
+@RequestMapping("/problems")
 public class ProblemController {
+    private final ProblemService problemService;
+    @GetMapping("/{id}")
+    public ProblemDto getProblem(@PathVariable long id) {
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProblem(@PathVariable long id) {
+
+    }
 }
--- src/main/java/com/example/Project_week6/service/ProblemService.java
+++ src/main/java/com/example/Project_week6/service/ProblemService.java
@@ -1,4 +1,6 @@
 package com.example.Project_week6.service;
 
 public class ProblemService {
+    private Map<long, ProblemDto> data;
+
 }
