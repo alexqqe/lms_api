68b6865b95912b30835205edb3e205f2872e1020
Deoksieze <husenko21@gmail.com>
2025-03-15 08:51:26 +0300

    Added TopicDto and TopicCreationDto

--- src/main/java/com/example/Project_week6/model/TopicCreationDto.java
+++ src/main/java/com/example/Project_week6/model/TopicCreationDto.java
@@ -1,4 +1,13 @@
 package com.example.Project_week6.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
 public class TopicCreationDto {
+    private String title;
+    private String text;
 }
