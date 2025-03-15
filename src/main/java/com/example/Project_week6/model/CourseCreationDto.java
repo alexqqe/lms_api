adb260a9575fb3b145d0f618cdd8be6c09ae6e6c
Deoksieze <husenko21@gmail.com>
2025-03-15 08:39:09 +0300

    Created CourseDto.java and CourseCreationDto.java

--- src/main/java/com/example/Project_week6/model/CourseCreationDto.java
+++ src/main/java/com/example/Project_week6/model/CourseCreationDto.java
@@ -1,4 +1,15 @@
 package com.example.Project_week6.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class CourseCreationDto {
+    private String title;
+    private String description;
 }
